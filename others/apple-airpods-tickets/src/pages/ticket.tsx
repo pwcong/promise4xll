e32b49ec6a80d85ca619a3576f9881829212a611
Pwcong <pwcong@foxmail.com>
2020-02-24 17:07:24 +0800

    update

--- others/apple-airpods-tickets/src/pages/ticket.tsx
+++ others/apple-airpods-tickets/src/pages/ticket.tsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 import './style/ticket.scss';
 
+export interface ITicket {
+  id: string
+  gift: string
+  description: string
+  call: string
+  isCompleted: boolean
+}
+
 export interface IProps {
   img?: string;
   gift: string
   description: string
   call: string
+  isCompleted?: boolean
+  onDone?: () => void
 }
 
 const Ticket: React.FunctionComponent<IProps> = props => {
-  const { img, gift, description, call } = props;
+  const { img, gift, description, call, onDone, isCompleted } = props;
 
   const cls = 'ticket';
   const clss = [cls];
@@ -29,9 +39,24 @@ const Ticket: React.FunctionComponent<IProps> = props => {
         <div className="ticket-bottom-left"></div>
         <div className="ticket-bottom-right"></div>
         <div className="ticket-bottom-top">{description}</div>
-        <a className="ticket-bottom-bottom" href={`tel:${call}`}>
-          <div className="ticket-button">立即使用</div>
-        </a>
+        <div className="ticket-bottom-bottom">
+          <div 
+            className={`ticket-button ${isCompleted ? 'ticket-button-disabled' : ''}`}
+            onClick={() => {
+              if (isCompleted) {
+                return
+              }
+              if (confirm('是否确认使用？')) {
+                const a = document.createElement('a')
+                a.href = `tel:${call}`
+                a.click()
+                onDone && onDone()
+              }
+            }}
+          >
+            { isCompleted ? '已使用' : '立即使用'}
+          </div>
+        </div>
       </div>
     </div>
   );
